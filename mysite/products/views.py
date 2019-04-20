8073ecd6783e8d9f819db48f90561e0b31869746
Yash Maheshwari <yash.maheshwari@thepsi.com>
2019-04-20 21:42:49 +0530

    created customized admin panel and templates

--- mysite/products/views.py
+++ mysite/products/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Product, Offer
 
 
 def index(request):
-    return HttpResponse('Hello surbhi')
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
 
 
 def new(request):
